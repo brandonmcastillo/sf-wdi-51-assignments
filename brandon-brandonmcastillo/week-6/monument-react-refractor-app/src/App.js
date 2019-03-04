5b601ac1213a2acde53b970e98e04d4e511d06c8
brandonmcastillo <brandonmaerina@gmail.com>
2019-03-04 01:25:04 -0800

    Finish Blog Sections

--- brandon-brandonmcastillo/week-6/monument-react-refractor-app/src/App.js
+++ brandon-brandonmcastillo/week-6/monument-react-refractor-app/src/App.js
@@ -1,33 +1,50 @@
 import React, { Component } from "react";
 import "./index.css";
 import "./app.css";
-import Navbar from "./Navbar/Navbar.js";
-import Header from "./Index/Header/Header.js";
-import Article from "./Index/Archive/Article.js";
-import About from "./Index/About/About.jsx";
-import Gallery from "./Index/Gallery/Gallery.js";
-import Form from "./Index/Contact/Contact.js";
-import Footer from "./Footer/Footer.js"
-import {BrowserRouter as Router} from 'react-router-dom';
+// import Navbar from "./Navbar/Navbar.js";
+// import Header from "./Index/Header/Header.js";
+// import Article from "./Index/Archive/Article.js";
+// import About from "./Index/About/About.jsx";
+// import Gallery from "./Index/Gallery/Gallery.js";
+// import Form from "./Index/Contact/Contact.js";
+// import Footer from "./Footer/Footer.js"
+// import {BrowserRouter as Router} from 'react-router-dom';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//         <div className="Main">
+//           <Navbar />
+//             <div className="Index">
+//               <Header />
+//               <Article />
+//               <About />
+//               <Gallery />
+//               <Form /> 
+//             </div>  
+//               <Footer/>
+//           </div>
+//         </Router>
+//     );
+//   }
+// }
+
+// export default App;
+
+// Blog code
+import HeaderBlog from './IndexBlog/Header/HeaderBlog'
+import Main from './IndexBlog/Main/Main'
+import ContactBlog from './IndexBlog/Contact/ContactBlog';
+
+const Blog = () => {
     return (
-      <Router>
-        <div className="Main">
-          <Navbar />
-            <div className="Index">
-              <Header />
-              <Article />
-              <About />
-              <Gallery />
-              <Form /> 
-            </div>  
-              <Footer/>
-          </div>
-        </Router>
-    );
-  }
+        <div>
+            <HeaderBlog />
+            <Main />
+            <ContactBlog />
+        </div>
+    )
 }
 
-export default App;
+export default Blog;
---
+++ brandon-brandonmcastillo/week-6/monument-react-refractor-app/src/IndexBlog/Contact/ContactBlog.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+import './Contact.css'
+
+const ContactBlog = () => {
+    return (
+     <section className="comment">
+		<div className="wrap">
+		        <h2>Leave a Comment</h2>
+			<form>
+				<input type="text" placeholder="Name" className="col-half" />
+				<input type="email" placeholder="Email" className="col-half" />
+				<textarea name="" id="" cols="30" rows="10" placeholder="Comment"></textarea>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	</section>
+    )
+}
+
+export default ContactBlog;
